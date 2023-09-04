d4e4dccba0bdf179441ce3d6a31af686e86fa470
PetarNeshkov <petio567@abv.bg>
2023-09-04 11:32:39 +0300

    Implemented first Thread exercise

---
+++ C-Sharp-Multithreading/01. Threads/Program.cs
@@ -0,0 +1,20 @@
+﻿MultipleThreads();
+
+void MultipleThreads()
+{
+    var thread = new Thread(() =>
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write("y");
+        }
+    });
+    
+    thread.Start();
+    
+    // Simultaneously, do something on the main thread.
+    for (int i = 0; i < 1000; i++)
+    {
+        Console.Write("x"); 
+    }
+}

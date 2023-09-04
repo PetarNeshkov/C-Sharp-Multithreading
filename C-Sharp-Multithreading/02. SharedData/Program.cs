97a31fa8c2720dd7ec210c7a3177b8014e569b9b
PetarNeshkov <petio567@abv.bg>
2023-09-04 12:56:57 +0300

    Exercising sharing data

---
+++ C-Sharp-Multithreading/02. SharedData/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02._SharedData;
+
+public class Program
+{
+    private static bool done;
+
+    public static void Main()
+    {
+       // new Thread(Go).Start();
+            
+       // Go();
+
+        CapturedVariables();
+    }
+
+    private static void Go()
+    {
+        if (!done)
+        {
+            Console.WriteLine("Done");
+            done = true; // Put this statement before the Console writing to see a different result.
+        }
+    }
+
+    private static void CapturedVariables()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var current = i;
+            new Thread(() =>
+            {
+                Thread.Sleep(3000);
+                Console.Write(current);
+            }).Start();
+        }
+    }
+}

02c9ad4edebbefd112cd2ac3cc7268e9977baca9
PetarNeshkov <petio567@abv.bg>
2023-09-04 13:14:10 +0300

    Exercising join and sleep

---
+++ C-Sharp-Multithreading/03. JoinAndSleep/Program.cs
@@ -0,0 +1,17 @@
+﻿var thread = new Thread(Go);
+
+Console.WriteLine("Start");
+
+thread.Start();
+
+Console.WriteLine("Working");
+
+thread.Join();
+
+Console.WriteLine("Thread has ended!");
+
+void Go()
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Slept!");
+}

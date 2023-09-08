5e4b0ed69b5423d258b77537c28ca40b29ec0194
Petar Neshkov <petar.neshkov@softuni.bg>
2023-09-08 15:28:28 +0300

    Exercising thread pooling and async delegate

---
+++ C-Sharp-Multithreading/04. ThreadPooling/Program.cs
@@ -0,0 +1,50 @@
+﻿//ThreadPoolWork();
+
+//AsyncDelegates();
+
+TaskBasedWork();
+
+
+return;
+
+void TaskBasedWork()
+{
+    var task = Task.Run(() =>
+    {
+        Go(123);
+    });
+
+    task.Wait();
+}
+
+void AsyncDelegates()
+{
+    Func<string, int> method = Result;
+    var asyncResult = method.BeginInvoke("Test", null, null);
+
+    Console.WriteLine("In between");
+
+    var result = method.EndInvoke(asyncResult);
+
+    Console.WriteLine(result);
+}
+
+int Result(string text)
+{
+    Thread.Sleep(1000);
+    return text.Length;
+}
+
+void ThreadPoolWork()
+{
+    //equivalent of Task.Run(() => Go);
+    ThreadPool.QueueUserWorkItem(Go, 123);
+    
+}
+
+void Go(object? data)
+{
+    Console.WriteLine("From the thread pool! "+ data);
+
+    Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
+}

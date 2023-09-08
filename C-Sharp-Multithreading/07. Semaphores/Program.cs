c9c83090b75e62d742b0e19e313c9a957bb8ca48
Petar Neshkov <petar.neshkov@softuni.bg>
2023-09-08 16:11:12 +0300

    Exercised SemaphoreSlim

---
+++ C-Sharp-Multithreading/07. Semaphores/Program.cs
@@ -0,0 +1,23 @@
+﻿// Initial capacity of 3, maximum capacity of 3.
+
+SemaphoreSlim locker = new SemaphoreSlim(3, 3);
+
+for (int i = 1; i <= 10; i++)
+{
+    new Thread(Enter!).Start(i);
+}
+
+void Enter(object id)
+{
+    Console.WriteLine(id + " wants to enter");
+
+    locker.Wait(); // -1
+    
+    Console.WriteLine(id + " is in!");
+    
+    Thread.Sleep(1000 * (int)id); // can be here at
+
+    Console.WriteLine(id + " is leaving"); // a time.
+
+    locker.Release(); // +1
+}

3aff11482ade02a08708fd1b3a850d5ffe1a44fd
Petar Neshkov <petar.neshkov@softuni.bg>
2023-09-08 15:46:09 +0300

    Exercising locking threads

---
+++ C-Sharp-Multithreading/05. Locking/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> data = Enumerable.Range(0, 100).ToList();
+
+object locker = new object();
+
+for (int i = 0; i < 10; i++)
+{
+    var thread = new Thread(Work);
+    thread.Start();
+}
+
+Console.WriteLine(data.Count);
+
+return;
+void Work()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(i);
+        
+        
+        Thread.Sleep(500);
+
+        lock (locker)
+        {
+            if (data.Count > 50)
+            {
+                data.RemoveAt(data.Count -1);
+            }
+        }
+    }
+}

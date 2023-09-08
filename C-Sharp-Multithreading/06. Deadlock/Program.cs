bd6c36b8ae99a0f880d9680da31b03becbcdb9fc
Petar Neshkov <petar.neshkov@softuni.bg>
2023-09-08 15:59:58 +0300

    Exercised deadlock

---
+++ C-Sharp-Multithreading/06. Deadlock/Program.cs
@@ -0,0 +1,31 @@
+﻿object FirstLocker = new object();
+object SecondLocker = new object();
+
+var t1 = new Thread(DeadLock1);
+var t2 = new Thread(DeadLock2);
+
+t1.Start();
+t2.Start();
+
+void DeadLock1()
+{
+    Console.WriteLine("Thread 1 got locked");
+    lock (SecondLocker)
+    {
+        Console.WriteLine("Thread 2 got locked");
+    }
+}
+
+void DeadLock2()
+{
+    lock (FirstLocker)
+    {
+        Console.WriteLine("Thread 1 got locked");
+        Thread.Sleep(500);
+
+        lock (SecondLocker)
+        {
+            Console.WriteLine("Thread 2 got locked");
+        }
+    }
+}

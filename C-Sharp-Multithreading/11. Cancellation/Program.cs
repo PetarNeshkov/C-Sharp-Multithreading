90bd6aed8627c693556802c5781dcab4354505b6
PetarNeshkov <petio567@abv.bg>
2023-09-11 00:35:23 +0300

    Exercised cancellation token

---
+++ C-Sharp-Multithreading/11. Cancellation/Program.cs
@@ -0,0 +1,36 @@
+﻿var tokenSource = new CancellationTokenSource();
+
+var thread = new Thread(() =>
+{
+   LongOperation(tokenSource.Token);
+});
+
+thread.Start();
+
+Console.WriteLine("Enter 'stop' to cancel...");
+
+while (true)
+{
+   var line = Console.ReadLine();
+   if (line?.ToLower() == "stop")
+   {
+      tokenSource.Cancel();
+      break;
+   }
+}
+
+void LongOperation(CancellationToken cancellationToken)
+{
+   for (int i = 0; i < 10; i++)
+   {
+      if (cancellationToken.IsCancellationRequested)
+      {
+         return;
+      }
+      
+      Thread.Sleep(1000);
+      Console.WriteLine("Working...");
+   }
+
+   Console.WriteLine("Finished!");
+}

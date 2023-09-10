d6efdfb48365124c2c3295f311010f3973aad1af
PetarNeshkov <petio567@abv.bg>
2023-09-11 01:49:45 +0300

    Implemented ReaderWriterLock

---
+++ C-Sharp-Multithreading/15. ReaderWriterLock/Program.cs
@@ -0,0 +1,62 @@
+﻿ReaderWriterLockSlim locker = new ReaderWriterLockSlim();
+List<int> items = new List<int>();
+Random random = new Random();
+
+new Thread(Read).Start(1);
+new Thread(Read).Start(2);
+new Thread(Read).Start(3);
+
+new Thread(Write).Start("A");
+new Thread(Write).Start("B");
+
+void Read(object? threadId)
+{
+    while (true)
+    {
+        try
+        {
+            locker.EnterReadLock();
+
+            foreach (var item in items)
+            {
+                Console.WriteLine("Thread " + threadId + " read " + item);
+                Thread.Sleep(100);
+            }
+        }
+        finally
+        {
+            locker.ExitReadLock();
+        }
+    }
+}
+
+void Write(object? threadId)
+{
+    while (true)
+    {
+        var newNumber = GetRandNum(100);
+
+        try
+        {
+            locker.EnterWriteLock();
+
+            items.Add(newNumber);
+        }
+        finally
+        {
+            locker.ExitWriteLock();
+        }
+
+
+        Console.WriteLine("Thread " + threadId + " added " + newNumber);
+        Thread.Sleep(100);
+    }
+}
+
+int GetRandNum(int max)
+{
+    lock (random)
+    {
+        return random.Next(max);
+    }
+}

d3975b1f9939f846f7896dfbb3dbad282d7c2a1a
PetarNeshkov <petio567@abv.bg>
2023-09-11 01:33:07 +0300

    Implemented Barrier

---
+++ C-Sharp-Multithreading/14. Barrier/Program.cs
@@ -0,0 +1,15 @@
+﻿Barrier barrier = new Barrier(3, _ => Console.WriteLine());
+
+new Thread(Speak).Start();
+new Thread(Speak).Start();
+new Thread(Speak).Start();
+
+void Speak()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(i + " ");
+        Thread.Sleep(1000);
+        barrier.SignalAndWait();
+    }
+}

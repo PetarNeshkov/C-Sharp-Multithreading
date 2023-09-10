d26d11846ef4838cb47edb49bdfe505de2217d78
PetarNeshkov <petio567@abv.bg>
2023-09-10 21:11:06 +0300

    Exercised timer

---
+++ C-Sharp-Multithreading/09. Timer/Program.cs
@@ -0,0 +1,10 @@
+﻿Timer timer = new Timer(DoWork, null, 5000, 1000);
+
+Console.ReadLine();
+
+timer.Dispose(); // Stops the timer and cleans up.
+
+void DoWork(object? work)
+{
+  Console.WriteLine("Repetitive work executed at: " + DateTime.Now);
+}

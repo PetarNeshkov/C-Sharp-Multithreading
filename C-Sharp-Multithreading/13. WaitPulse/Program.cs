da7872630a3222c665a96f783ebf20c5dd60a852
PetarNeshkov <petio567@abv.bg>
2023-09-11 01:25:45 +0300

    Implemented wait/pulse

---
+++ C-Sharp-Multithreading/13. WaitPulse/Program.cs
@@ -0,0 +1,28 @@
+﻿object locker = new();
+bool go = false;
+
+// The new thread will block
+new Thread(Work).Start(); // because _go==false.
+
+Console.ReadLine(); // Wait for user to hit Enter
+
+
+lock (locker)
+{
+    go = true; // Let's now wake up the thread by
+    // setting _go=true and pulsing.
+    Monitor.Pulse(locker);
+}
+
+void Work()
+{
+    lock (locker)
+    {
+        while (!go)
+        {
+            Monitor.Wait(locker); // Lock is released while we’re waiting
+        }
+    }
+
+    Console.WriteLine("Woken!!!");
+}

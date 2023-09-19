e9881a42a74940c9f7e68da1af982462f3468f7f
PetarNeshkov <petio567@abv.bg>
2023-09-19 21:23:21 +0300

    Implemented task

---
+++ C-Sharp-Multithreading/20. DivideAndConquer/AsyncFileSearcher.cs
@@ -0,0 +1,100 @@
+﻿namespace DivideAndConquer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class AsyncFileSearcher
+    {
+        private readonly string fileContent;
+
+        public AsyncFileSearcher(string filePath)
+            => this.fileContent = File.ReadAllText(filePath);
+
+        public async Task<int> Search(string searchTerm)
+        {
+            var totalProcessors = Environment.ProcessorCount;
+
+            var totalLength = this.fileContent.Length;
+
+            var partLength = (int)Math.Ceiling((double)totalLength / totalProcessors);
+
+            var count = 0;
+
+            var tasks = new List<Task>();
+
+            for (var i = 0; i < totalProcessors; i++)
+            {
+                var current = i;
+
+                var task = Task.Run(() =>
+                {
+                    var (startIndex, endIndex) = this.GetPartIndices(current, partLength);
+
+                    var threadCount = 0;
+
+                    var currentIndex = startIndex - 1;
+
+                    while (true)
+                    {
+                        currentIndex = this.fileContent.IndexOf(
+                            searchTerm,
+                            currentIndex + 1,
+                            endIndex - currentIndex - 1,
+                            StringComparison.InvariantCulture);
+
+                        if (currentIndex < 0)
+                        {
+                            break;
+                        }
+
+                        threadCount++;
+                    }
+
+                    Interlocked.Add(ref count, threadCount);
+                });
+
+                tasks.Add(task);
+            }
+
+            for (var i = 0; i < totalProcessors - 1; i++)
+            {
+                var (_, firstEndIndex) = this.GetPartIndices(i, partLength);
+                var (secondStartIndex, _) = this.GetPartIndices(i + 1, partLength);
+
+                var mergedStartIndex = firstEndIndex - (searchTerm.Length - 1);
+                var mergedEndIndex = secondStartIndex + (searchTerm.Length - 1);
+
+                var found = this.fileContent.IndexOf(
+                    searchTerm,
+                    mergedStartIndex,
+                    mergedEndIndex - mergedStartIndex - 1,
+                    StringComparison.InvariantCulture);
+
+                if (found > -1)
+                {
+                    Interlocked.Increment(ref count);
+                }
+            }
+
+            await Task.WhenAll(tasks);
+
+            return count;
+        }
+
+        private (int StartIndex, int EndIndex) GetPartIndices(int part, int partLength)
+        {
+            var startIndex = part * partLength;
+            var endIndex = (part + 1) * partLength;
+
+            if (endIndex > this.fileContent.Length - 1)
+            {
+                endIndex = this.fileContent.Length;
+            }
+
+            return (startIndex, endIndex);
+        }
+    }
+}
---
+++ C-Sharp-Multithreading/20. DivideAndConquer/ThreadFileSearcher.cs
@@ -0,0 +1,102 @@
+﻿namespace DivideAndConquer
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+
+    public class ThreadFileSearcher
+    {
+        private readonly string fileContent;
+
+        public ThreadFileSearcher(string filePath)
+            => this.fileContent = File.ReadAllText(filePath);
+
+        public int Search(string searchTerm)
+        {
+            var totalProcessors = Environment.ProcessorCount;
+            var countdown = new CountdownEvent(totalProcessors);
+
+            var totalLength = this.fileContent.Length;
+
+            var partLength = (int)Math.Ceiling((double)totalLength / totalProcessors);
+
+            var count = 0;
+
+            for (int i = 0; i < totalProcessors; i++)
+            {
+                var current = i;
+
+                var thread = new Thread(() =>
+                {
+                    var (startIndex, endIndex) = this.GetPartIndices(current, partLength);
+
+                    var threadCount = 0;
+
+                    var currentIndex = startIndex - 1;
+
+                    while (true)
+                    {
+                        currentIndex = this.fileContent.IndexOf(
+                            searchTerm,
+                            currentIndex + 1,
+                            endIndex - currentIndex - 1,
+                            StringComparison.InvariantCulture);
+
+                        if (currentIndex < 0)
+                        {
+                            break;
+                        }
+
+                        threadCount++;
+                    }
+
+                    Interlocked.Add(ref count, threadCount);
+
+                    countdown.Signal();
+                })
+                {
+                    Priority = ThreadPriority.Highest
+                };
+
+                thread.Start();
+            }
+
+            for (var i = 0; i < totalProcessors - 1; i++)
+            {
+                var (_, firstEndIndex) = this.GetPartIndices(i, partLength);
+                var (secondStartIndex, _) = this.GetPartIndices(i + 1, partLength);
+
+                var mergedStartIndex = firstEndIndex - (searchTerm.Length - 1);
+                var mergedEndIndex = secondStartIndex + (searchTerm.Length - 1);
+
+                var found = this.fileContent.IndexOf(
+                    searchTerm,
+                    mergedStartIndex,
+                    mergedEndIndex - mergedStartIndex - 1,
+                    StringComparison.InvariantCulture);
+
+                if (found > -1)
+                {
+                    Interlocked.Increment(ref count);
+                }
+            }
+
+            countdown.Wait();
+
+            return count;
+        }
+
+        private (int StartIndex, int EndIndex) GetPartIndices(int part, int partLength)
+        {
+            var startIndex = part * partLength;
+            var endIndex = (part + 1) * partLength;
+
+            if (endIndex > this.fileContent.Length - 1)
+            {
+                endIndex = this.fileContent.Length;
+            }
+
+            return (startIndex, endIndex);
+        }
+    }
+}

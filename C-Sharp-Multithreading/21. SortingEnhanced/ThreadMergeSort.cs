34fff3aa8577e5ace899b441b7a44ac11af8fc99
PetarNeshkov <petio567@abv.bg>
2023-09-21 23:36:11 +0300

    Implemented quick and merge sort parallel

---
+++ C-Sharp-Multithreading/21. SortingEnhanced/ThreadMergeSort.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SortingEnhanced
+{
+    using System;
+
+    public static class ThreadMergeSort
+    {
+        public static void ParallelSort(int[] input, int left, int right)
+        {
+            if (left < right)
+            {
+                var middle = (left + right) / 2;
+
+                if (right - left < 20000)
+                {
+                    Sort(input, left, middle);
+                    Sort(input, middle + 1, right);
+                    
+                    return;
+                }
+                
+                // Slower variant
+                
+                // else
+                // {
+                //     Parallel.Invoke(
+                //         () => ParallelSort(input, left, middle),
+                //         () => ParallelSort(input, middle + 1, right));
+                // }
+                
+                // Fallback to normal merge sort.
+                if (input.Length < 20000)
+                {
+                    Sort(input, 0, input.Length - 1);
+                    return;
+                }
+
+                var threads = 8;
+
+                var partIndices = CalculatePartIndices(input, threads);
+
+                SortThreadsParts(input, threads, partIndices);
+                MergeFinalParts(input, partIndices);
+            }
+        }
+        
+        private static void SortThreadsParts(int[] input, int threads, List<int> partIndices)
+        {
+            var countdown = new CountdownEvent(threads);
+
+            for (int i = 0; i < partIndices.Count - 1; i++)
+            {
+                var left = partIndices[i] + 1;
+                var right = partIndices[i + 1];
+
+                var thread = new Thread(() =>
+                {
+                    Sort(input, left, right);
+                    countdown.Signal();
+                });
+
+                thread.Start();
+            }
+
+            countdown.Wait();
+        }
+        
+        private static List<int> CalculatePartIndices(int[] input, int threads)
+        {
+            var partLength = (int)Math.Ceiling((double)input.Length / threads);
+            var partIndices = new List<int> { -1 };
+
+            var done = false;
+
+            while (!done)
+            {
+                var next = partIndices[^1] + partLength;
+
+                if (next >= input.Length - 1)
+                {
+                    next = input.Length - 1;
+                    done = true;
+                }
+
+                partIndices.Add(next);
+            }
+
+            return partIndices;
+        }
+        
+        private static void MergeFinalParts(int[] input, List<int> partIndices)
+        {
+            var increment = 1;
+            var done = false;
+
+            while (!done)
+            {
+                var count = 0;
+
+                while (count != partIndices.Count - 1)
+                {
+                    var left = partIndices[count] + 1;
+                    count += increment;
+                    var middle = partIndices[count];
+                    count += increment;
+                    var right = partIndices[count];
+
+                    Merge(input, left, middle, right);
+
+                    if (left == 0 && right == input.Length - 1)
+                    {
+                        done = true;
+                        break;
+                    }
+                }
+
+                increment *= 2;
+            }
+
+            // Merge(input, partIndices[0] + 1, partIndices[1], partIndices[2]);
+            // Merge(input, partIndices[2] + 1, partIndices[3], partIndices[4]);
+            // Merge(input, partIndices[4] + 1, partIndices[5], partIndices[6]);
+            // Merge(input, partIndices[6] + 1, partIndices[7], partIndices[8]);
+               
+            // Merge(input, partIndices[0] + 1, partIndices[2], partIndices[4]);
+            // Merge(input, partIndices[4] + 1, partIndices[6], partIndices[8]);
+               
+            // Merge(input, partIndices[0] + 1, partIndices[4], partIndices[8]);
+        }
+        
+        public static void Sort(int[] input, int left, int right)
+        {
+            if (left < right)
+            {
+                var middle = (left + right) / 2;
+
+                Sort(input, left, middle);
+                Sort(input, middle + 1, right);
+
+                Merge(input, left, middle, right);
+            }
+        }
+
+        private static void Merge(int[] input, int left, int middle, int right)
+        {
+            var leftArray = new int[middle - left + 1];
+            var rightArray = new int[right - middle];
+
+            Array.Copy(input, left, leftArray, 0, middle - left + 1);
+            Array.Copy(input, middle + 1, rightArray, 0, right - middle);
+
+            var i = 0;
+            var j = 0;
+
+            for (var k = left; k < right + 1; k++)
+            {
+                if (i == leftArray.Length)
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+                else if (j == rightArray.Length)
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else if (leftArray[i] <= rightArray[j])
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ C-Sharp-Multithreading/21. SortingEnhanced/ThreadQuickSort.cs
@@ -0,0 +1,102 @@
+﻿namespace SortingEnhanced
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class ThreadQuickSort
+    {
+        public static void ParallelSort(int[] input, int low, int high)
+        {
+            // Fallback to normal quick sort.
+            if (high - low < 20000)
+            {
+                Sort(input, low, high);
+            }
+            else
+            {
+                var part = Partition(input, low, high);
+
+                //Slower variant
+                
+                // Parallel.Invoke(
+                //     () => ParallelSort(input, low, part - 1),
+                //     () => ParallelSort(input, part + 1, high));
+
+                var countdown = new CountdownEvent(2);
+
+                var leftThread = new Thread(() =>
+                {
+                    ParallelSort(input, low, part - 1);
+                    countdown.Signal();
+                });
+
+                var rightThread = new Thread(() =>
+                {
+                    ParallelSort(input, part + 1, high);
+                    countdown.Signal();
+                });
+
+                leftThread.Start();
+                rightThread.Start();
+
+                countdown.Wait();
+            }
+        }
+
+        public static void Sort(int[] input, int low, int high)
+        {
+            if (high - low < 40)
+            {
+                InsertionSort(input, low, high);
+            }
+            else
+            {
+                var part = Partition(input, low, high);
+                Sort(input, low, part - 1);
+                Sort(input, part + 1, high);
+            }
+        }
+
+        private static int Partition(int[] input, int low, int high)
+        {
+            var j = low;
+            var pivot = input[low];
+
+            for (int i = low; i <= high; i++)
+            {
+                if (input[i].CompareTo(pivot) >= 0)
+                {
+                    continue;
+                }
+
+                j++;
+                Swap(input, i, j);
+            }
+
+            Swap(input, low, j);
+            return j;
+        }
+
+        private static void InsertionSort(int[] input, int low, int high)
+        {
+            for (var i = low + 1; i <= high; i++)
+            {
+                var j = i - 1;
+                var x = input[i];
+
+                while (j >= 0 && input[j].CompareTo(x) > 0)
+                {
+                    input[j + 1] = input[j];
+                    j--;
+                }
+
+                input[j + 1] = x;
+            }
+        }
+
+        private static void Swap(int[] input, int i, int j)
+        {
+            (input[i], input[j]) = (input[j], input[i]);
+        }
+    }
+}

fa15d21c2609a3854a2f30fd12c2e590677bdb30
PetarNeshkov <petio567@abv.bg>
2023-09-22 00:25:56 +0300

    Implemented task with cache - threads

---
+++ C-Sharp-Multithreading/22. ServerCache/MyCache.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+
+namespace MyCache
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class MyCache
+    {
+        private readonly Dictionary<string, object> cache;
+        private readonly SemaphoreSlim locker;
+
+        public MyCache()
+        {
+            this.cache = new Dictionary<string, object>();
+            this.locker = new SemaphoreSlim(1);
+        }
+
+        public TItem GetOrCreate<TItem>(string key, Func<TItem> createItem)
+            where TItem : class
+        {
+            locker.Wait();
+
+            try
+            {
+                if (!cache.ContainsKey(key))
+                {
+                    cache[key] = createItem();
+                }
+            }
+            finally
+            {
+                locker.Release();
+            }
+
+            return cache[key] as TItem;
+        }
+
+        public async Task<TItem> GetOrCreateAsync<TItem>(string key, Func<Task<TItem>> createItem)
+            where TItem : class
+        {
+            await locker.WaitAsync();
+
+            try
+            {
+                if (!cache.ContainsKey(key))
+                {
+                    cache[key] = await createItem();
+                }
+            }
+            finally
+            {
+                locker.Release();
+            }
+            
+            return  cache[key] as TItem;
+        }
+    }
+}
---
+++ C-Sharp-Multithreading/22. ServerCache/MyConcurrentCache.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading;
+
+namespace MyCache
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading.Tasks;
+
+    public class MyConcurrentCache
+    {
+        private readonly ConcurrentDictionary<string, object> cache;
+        private readonly ConcurrentDictionary<string, SemaphoreSlim> lockers;
+
+        public MyConcurrentCache()
+        {
+            this.cache = new ConcurrentDictionary<string, object>();
+            this.lockers = new ConcurrentDictionary<string, SemaphoreSlim>();
+        }
+
+        public TItem GetOrCreate<TItem>(string key, Func<TItem> createItem)
+            where TItem : class
+        {
+            if (!cache.TryGetValue(key, out var value ))
+            {
+                var locker = lockers.GetOrAdd(key, _ => new SemaphoreSlim(1));
+
+                locker.Wait();
+
+                try
+                {
+                    if (!cache.ContainsKey(key))
+                    {
+                        value = cache.GetOrAdd(key, _ => createItem());
+                    }
+                }
+                finally
+                {
+                    locker.Release();
+                }
+            }
+
+            return value as TItem;
+        }
+
+        public async Task<TItem> GetOrCreateAsync<TItem>(string key, Func<Task<TItem>> createItem)
+            where TItem : class
+        {
+            if (!cache.TryGetValue(key, out var value ))
+            {
+                var locker = lockers.GetOrAdd(key, _ => new SemaphoreSlim(1));
+
+               await locker.WaitAsync();
+
+                try
+                {
+                    if (!cache.ContainsKey(key))
+                    {
+                        value = cache.GetOrAdd(key, _ => createItem());
+                    }
+                }
+                finally
+                {
+                    locker.Release();
+                }
+            }
+
+            return value as TItem;
+        }
+    }
+}

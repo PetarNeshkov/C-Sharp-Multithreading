9e278273412c3936d204ec0cc0e1e181a8ce5743
PetarNeshkov <petio567@abv.bg>
2023-09-25 01:17:17 +0300

    Implemented queue synchronizer job

---
+++ C-Sharp-Multithreading/24. FolderSync/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FolderSync
+{
+    using System;
+    using System.Diagnostics;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            const string source = "Source";
+            const string destination = "Destination";
+
+            using (var process = Process.GetCurrentProcess())
+            {
+                process.PriorityClass = ProcessPriorityClass.BelowNormal;
+            }
+
+            var folderSynchronizer = new BlockingCollectionFolderSynchronizer(source, destination);
+
+            folderSynchronizer.Start();
+
+            Console.WriteLine($"Folder '{destination}' is now synchronized with '{source}'.");
+            Console.WriteLine("Enter 'stop' to end syncing...");
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line?.ToLower() == "stop")
+                {
+                    folderSynchronizer.Stop();
+                    break;
+                }
+            }
+        }
+    }
+}
